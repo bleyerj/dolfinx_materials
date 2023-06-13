86918c6b76a2919d7b3248422a0610e27602cd8b
Jeremy Bleyer <jeremy.bleyer@enpc.fr>
2023-06-13 12:38:54 +0200

    Add setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Setup file for the dolfinx_materials package.
+
+@author: Jeremy Bleyer, Ecole des Ponts ParisTech,
+Laboratoire Navier (ENPC, Univ Gustave Eiffel, CNRS, UMR 8205)
+@email: jeremy.bleyer@enpc.fr
+"""
+from setuptools import setup
+
+setup()
