87c6aa7a322465e1cd1f73627ff9389fe0f2e3ef
Jeremy Bleyer <jeremy.bleyer@enpc.fr>
2024-08-21 16:39:15 +0200

    Improve JAXNewton

--- dolfinx_materials/jax_newton_solver.py
+++ dolfinx_materials/jax_newton_solver.py
@@ -17,7 +17,8 @@ from typing import NamedTuple, Any
 class SolverParameters(NamedTuple):
     """Parameters of Newton solver"""
 
-    tol: float
+    rtol: float
+    atol: float
     niter_max: int
 
 
@@ -30,6 +31,16 @@ class SolverState(NamedTuple):
     value: Any
 
 
+@jax.jit
+def _solve_linear_system(x, J, b):
+    if jnp.isscalar(x):
+        return b / J
+    else:
+        # dx, info = jax.scipy.sparse.linalg.cg(J, b)
+        dx = jnp.linalg.solve(J, b)
+    return dx
+
+
 def newton_solve(x, r, dr_dx, params):
     niter = 0
 
@@ -37,19 +48,12 @@ def newton_solve(x, r, dr_dx, params):
     norm_res0 = jnp.linalg.norm(res)
 
     @jax.jit
-    def _solve_linear_system(x, b):
-        if jnp.isscalar(x):
-            return b / dr_dx(x)
-        else:
-            J = dr_dx(x)
-            dx = jax.scipy.linalg.solve(J, b)
-        return dx
-
-    @jax.jit
     def cond_fun(state):
         norm_res, niter, _ = state
         return jnp.logical_and(
-            norm_res > params.tol * norm_res0, niter < params.niter_max
+            jnp.logical_and(norm_res > params.atol, norm_res > params.rtol * norm_res0),
+            # norm_res > params.rtol * norm_res0,
+            niter < params.niter_max,
         )
 
     @jax.jit
@@ -58,11 +62,12 @@ def newton_solve(x, r, dr_dx, params):
 
         x, res = history
 
-        dx = _solve_linear_system(x, -res)
+        dx = _solve_linear_system(x, dr_dx(x), -res)
         x += dx
 
         res = r(x)
         norm_res = jnp.linalg.norm(res)
+
         history = x, res
         niter += 1
 
@@ -77,42 +82,42 @@ def newton_solve(x, r, dr_dx, params):
 
 
 class JAXNewton:
-    """A tiny Newton solver implemented in JAX."""
+    """A tiny Newton solver implemented in JAX. Derivatives are computed via custom implicit differentiation."""
 
-    def __init__(self, r, dr_dx=None, tol=1e-8, niter_max=200):
+    def __init__(self, r, dr_dx=None, rtol=1e-8, atol=1e-8, niter_max=2000):
         """Newton solver for a vector of residual r(x).
 
         Parameters
         ----------
-        r : callable
-            Residual to solve for. r(x) is a function of R^n to R^n, n>=1.
+        r : callable, list, tuple
+            Residual to solve for. r(x) is a function of R^n to R^n. Alternatively, r can be a list/tuple
+            of functions with the same signature. The resulting system corresponds to a concatenation of all
+            individual residuals.
         dr_dx : callable, optional
             The jacobian of the residual. dr_dx(x) is a function of R^n to R^{n}xR^n. If None (default),
             JAX computes the residual using forward-mode automatic differentiation.
-        tol :float, optional
+        rtol : float, optional
             Relative tolerance for the Newton method, by default 1e-8
+        atol : float, optional
+            Absolute tolerance for the Newton method, by default 1e-8
         niter_max : int, optional
             Maximum number of allowed iterations, by default 200
         """
-        self.params = SolverParameters(tol, niter_max)
+        self.params = SolverParameters(rtol, atol, niter_max)
         # residual
-        self.r = r
+        if isinstance(r, list) or isinstance(r, tuple):
+            self.r = lambda x: jnp.concatenate([jnp.atleast_1d(ri(x)) for ri in r])
+        else:
+            self.r = r
         if dr_dx is None:
-            self.dr_dx = jax.jit(jax.jacfwd(r))
+            self.dr_dx = jax.jit(jax.jacfwd(self.r))
         else:
             self.dr_dx = dr_dx
-
-    @property
-    def jacobian(self):
-        return self.dr_dx
 
     @partial(jax.jit, static_argnums=(0,))
     def solve(self, x):
         solve = lambda f, x: newton_solve(x, f, jax.jacfwd(f), self.params)[0]
 
-        if jnp.ndim(x) == 0:
-            tangent_solve = lambda g, y: y / g(1.0)
-        else:
-            tangent_solve = lambda g, y: jax.scipy.linalg.solve(jax.jacfwd(g)(y), y)
+        tangent_solve = lambda g, y: _solve_linear_system(x, jax.jacfwd(g)(y), y)
         x_sol = jax.lax.custom_root(self.r, x, solve, tangent_solve)
         return x_sol, self.r(x_sol)
