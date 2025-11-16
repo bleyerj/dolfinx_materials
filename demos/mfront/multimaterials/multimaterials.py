5dc2507268390bb2c7b3e2023abb2d5b3b5d13ae
Jeremy Bleyer <jeremy.bleyer@enpc.fr>
2025-11-16 01:23:07 +0100

    Working mixed MFront-jaxmat demo

--- demos/mfront/multimaterials/multimaterials.py
+++ demos/mfront/multimaterials/multimaterials.py
@@ -1,17 +1,19 @@
 # ---
 # jupyter:
 #   jupytext:
+#     formats: md:myst,py:percent,ipynb
 #     text_representation:
 #       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.1
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.18.1
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: fenicsx-v0.10
 #     language: python
 #     name: python3
 # ---
 
+# %% [markdown]
 # # Multiple behaviors on subdomains and interface conditions
 #
 # In this demo, we show how to define a problem containing different subdomains, potentially separated by an interface. In each subdomain, a different MFront behavior is called.
@@ -33,9 +35,10 @@
 # * {Download}`Python script<./multimaterials.py>`
 # * {Download}`Jupyter notebook<./multimaterials.ipynb>`
 # * {Download}`Utility module<./utils.py>`
-# * {Download}`MFront files<./behaviors.zip>`
+# * {Download}`MFront file 1<./IsotropicPlasticMisesFlowVoce.mfront>` {Download}`MFront file 2<./IsotropicPlasticHosfordFlowLinear.mfront>`
 # ```
 
+# %%
 import numpy as np
 from mpi4py import MPI
 import gmsh
@@ -46,18 +49,20 @@ from dolfinx_materials.quadrature_map import QuadratureMap
 from dolfinx_materials.mfront import MFrontMaterial
 from dolfinx_materials.solvers import NonlinearMaterialProblem
 
+# %%
 from utils import (
     interface_int_entities,
     transfer_meshtags_to_submesh,
-    interpolate_submesh_to_parent,
 )
 
 
+# %% [markdown]
 # ## Meshing and subdomains
+#
 # We first create the mesh and define the different tags for identifying physical domains and interfaces.
 
 
-# + tags=["hide-input"]
+# %% tags=["hide-input"]
 def create_matrix_inclusion_mesh(L, W, R, hsize):
     comm = MPI.COMM_WORLD
 
@@ -102,7 +107,7 @@ def create_matrix_inclusion_mesh(L, W, R, hsize):
     return (domain, cells, facets)
 
 
-# +
+# %%
 length = 1.0
 width = 0.5
 radius = 0.25
@@ -117,12 +122,13 @@ interface_facets = facets.find(INT_TAG)
 
 tdim = domain.topology.dim
 fdim = tdim - 1
-# -
 
+# %% [markdown]
 # ### Submesh creation
 #
 # We define three submeshes: two submeshes (of codim. 0) corresponding to the matrix and inclusion 2D domains and one submesh (of codim. 1) corresponding to the facet restriction on the interface
 
+# %%
 subdomain2, subdomain2_cell_map, subdomain2_vertex_map, _ = mesh.create_submesh(
     domain, tdim, cells.find(INCL_TAG)
 )
@@ -133,8 +139,10 @@ interface_mesh, interface_cell_map, _, _ = mesh.create_submesh(
     domain, fdim, interface_facets
 )
 
+# %% [markdown]
 # Now that we have defined submeshes, we need to transfer (facets) meshtags from those defined on the original domain to their subdomain counterpart. This function is available in {download}`./utils.py`.
 
+# %%
 subdomain1_facet_tags, subdomain1_facet_map = transfer_meshtags_to_submesh(
     domain, facets, subdomain1, subdomain1_vertex_map, subdomain1_cell_map
 )
@@ -142,18 +150,17 @@ subdomain2_facet_tags, subdomain2_facet_map = transfer_meshtags_to_submesh(
     domain, facets, subdomain2, subdomain2_vertex_map, subdomain2_cell_map
 )
 
+# %% [markdown]
 # # ### Entity map and integration measures
-# #
-# # Similarly to the previous CZM tour, *entity maps* must be defined to link integration of quantities defined on the subdomains.
+#
+# Similarly to the previous CZM tour, *entity maps* must be defined to link integration of quantities defined on the subdomains.
+#
+# Before setting up the `entity_maps` list, we need a specific treatment for integrating terms on the interface.
+# The `interface_int_integration` manually defines the specific integration quantities on the interface. Besides, interface terms seen from one specific subdomain only exist on one side. The helper function defines a consistent ordering such that cells for which `marker[cell] != 0` correspond to the `"+"` restriction (inclusions here), and cells for which `marker[cell] == 0` correspond to the `"-"` restriction (matrix here).
 
-
+# %%
 subdomain1.topology.create_connectivity(fdim, tdim)
 subdomain2.topology.create_connectivity(fdim, tdim)
-
-# # Before setting up the `entity_maps` dictionary, we need a specific treatment for integrating terms on the interface.
-# The `interface_int_integration` manually defines integration quantities on the interface. Besides, interface terms
-# seen from one specific subdomain only exist on one side. As the assembler complains about this, there is a specific
-#  tweak to map cells from one side of the interface to the other side, thereby modifying the `domain_to_subdomain` maps. Most importantly, cells in subdomain 1 correspond to the `"+"` side of the interface and cells in subdomain 2 to the `"-"` side.
 
 # Create a marker to identify cells on the "+" side of the interface
 cell_imap = domain.topology.index_map(tdim)
@@ -162,11 +169,17 @@ marker = np.zeros(num_cells)
 marker[cells.find(INCL_TAG)] = 1
 interface_entities = interface_int_entities(domain, interface_facets, marker)
 
+# %% [markdown]
+# Finally, the list of entity maps relating both subdomains and the interface to the parent mesh is defined.
+
+# %%
 entity_maps = [subdomain1_cell_map, subdomain2_cell_map, interface_cell_map]
 # -
 
+# %% [markdown]
 # We are now in position to define the various integration measures. The key point here is that the `dInt` interface measure is defined using prescribed integration entities which have been defined earlier. This is done by passing them to `subdomain_data` as follows.
 
+# %%
 dx = ufl.Measure("dx", domain=domain, subdomain_data=cells)
 ds = ufl.Measure("ds", domain=domain, subdomain_data=facets)
 dx_int = ufl.Measure("dx", domain=interface_mesh)
@@ -178,14 +191,15 @@ dInt = ufl.Measure(
 )
 
 
+# %% [markdown]
 # ## Nonlinear behavior formulation
 # $\newcommand{\bu}{\boldsymbol{u}}
 # \newcommand{\bv}{\boldsymbol{v}}
 # \newcommand{\jump}[1]{[\![#1]\!]}$
-# We now define the relevant function spaces. As hinted before, the unknown $\bu$ will consist of two displacements $(\bu^{(1)},\bu^{(2)})$ respectively belonging to a continuous Lagrange space defined on subdomains 1 and 2. We use a `MixedFunctionSpace` for this, meaning that we will end up with a block system. For easier post-processing, the computed displacement will be stored as a `DG` function, with jumps being non zero only at the interface.
+# We now define the relevant function spaces. As hinted before, the unknown $\bu$ will consist of two displacements $(\bu^{(1)},\bu^{(2)})$ respectively belonging to a continuous Lagrange space defined on subdomains 1 and 2. We use a `MixedFunctionSpace` for this, meaning that we will end up with a block system.
 
 
-# +
+# %%
 def strain(u):
     return ufl.as_vector(
         [
@@ -201,23 +215,22 @@ def strain(u):
 
 V1 = fem.functionspace(subdomain1, ("Lagrange", 1, (tdim,)))
 V2 = fem.functionspace(subdomain2, ("Lagrange", 1, (tdim,)))
-V = fem.functionspace(domain, ("DG", 1, (tdim,)))  # for post-processing only
 W = ufl.MixedFunctionSpace(V1, V2)
-u1 = fem.Function(V1, name="Displacement_1")
-u2 = fem.Function(V2, name="Displacement_2")
-u = fem.Function(V, name="Displacement")
+u1 = fem.Function(V1, name="Displacement")
+u2 = fem.Function(V2, name="Displacement")
 v1, v2 = ufl.TestFunctions(W)
 du1, du2 = ufl.TrialFunctions(W)
-# -
 
+# %% [markdown]
 # ### Material laws on subdomains
 #
 # Second, we define two different `MFrontMaterial` on the two subdomains. In this example, we use two plastic behaviors with different yield surfaces and hardening laws. In the matrix, a von Mises criterion is used with an exponential Voce hardening whereas in the stiffer inclusions, we use a Hosford criterion and linear isotropic hardening.
 #
 # ```{important}
-# It is perfectly possible to use behaviors with different internal state variables, and even with different gradients/fluxes etc. They are really independent from each other and will only be combined by summing their contribution to the resulting weak form. As a result, we can also combine a MFront implementation and a JAX implementation for instance.
+# It is perfectly possible to use behaviors with different internal state variables, and even with different gradients/fluxes etc. They are really independent from each other and will only be combined by summing their contribution to the resulting weak form. As a result, we could also combine a MFront implementation and a JAX implementation for instance.
 # ```
 
+# %%
 material1 = MFrontMaterial(
     "src/libBehaviour.so",
     "IsotropicPlasticMisesFlowVoce",
@@ -229,20 +242,32 @@ material1 = MFrontMaterial(
         "b": 100,
     },
 )
-material2 = MFrontMaterial(
-    "src/libBehaviour.so",
-    "IsotropicPlasticHosfordFlowLinear",
-    material_properties={
-        "young_modulus": 90e3,
-        "poisson_ratio": 0.25,
-        "hardening_slope": 10.0,
-        "R0": 200.0,
-    },
+# material2 = MFrontMaterial(
+#     "src/libBehaviour.so",
+#     "IsotropicPlasticHosfordFlowLinear",
+#     material_properties={
+#         "young_modulus": 90e3,
+#         "poisson_ratio": 0.25,
+#         "hardening_slope": 10.0,
+#         "R0": 200.0,
+#     },
+# )
+from dolfinx_materials.jaxmat import JAXMaterial
+import jax.numpy as jnp
+import jaxmat.materials as jm
+import equinox as eqx
+
+elasticity = jm.LinearElasticIsotropic(E=90e3, nu=0.25)
+yield_stress = jm.VoceHardening(sig0=200.0, b=10.0, sigu=300.0)
+behavior = jm.vonMisesIsotropicHardening(
+    elasticity=elasticity, yield_stress=yield_stress
 )
+material2 = JAXMaterial(behavior)
 
-# As a result, we define two different `QuadratureMap` defined on both subdomains. Note that the registered gradients involve the two different displacements `u1` and `u2` respectively. Note that when assembling mixed forms it is more convenient that integration measures are defined on the similar parent domain (the full mesh) and to pass the entity_maps dictionary when compiling the forms. We thus redefine the `qmap` measures metadata accordingly. Finally, we define the contributions of both subdomains to the total residual form.
+# %% [markdown]
+# As a result, we define two different `QuadratureMap` defined on both subdomains. Note that the registered gradients involve the two different displacements `u1` and `u2` respectively. Note that when assembling mixed forms it is more convenient that integration measures are defined on the similar parent domain (the full mesh) and to pass the entity maps when compiling the forms. We thus redefine the `qmap` measures metadata accordingly. Finally, we define the contributions of both subdomains to the total residual form.
 
-# +
+# %%
 deg_quad = 1
 qmap1 = QuadratureMap(subdomain1, deg_quad, material1)
 qmap1.register_gradient("Strain", strain(u1))
@@ -250,16 +275,15 @@ qmap1.dx = qmap1.dx(domain=domain, subdomain_data=cells)
 sig1 = qmap1.fluxes["Stress"]
 
 qmap2 = QuadratureMap(subdomain2, deg_quad, material2)
-qmap2.register_gradient("Strain", strain(u2))
+qmap2.register_gradient("strain", strain(u2))
 qmap2.dx = qmap2.dx(domain=domain, subdomain_data=cells)
-sig2 = qmap2.fluxes["Stress"]
+sig2 = qmap2.fluxes["stress"]
 
 Res_matrix = ufl.dot(sig1, strain(v1)) * qmap1.dx(1)
 Res_inclusions = ufl.dot(sig2, strain(v2)) * qmap2.dx(2)
 
 
-# -
-
+# %% [markdown]
 # ### Interface behavior
 #
 # As regards the elastic interface of stiffness $K$, its contribution to the total residual is given by:
@@ -271,7 +295,7 @@ Res_inclusions = ufl.dot(sig2, strain(v2)) * qmap2.dx(2)
 # where we define the displacement $\jump{\bu} = \bu^{(2)} - \bu^{(1)}$ with $(1)$ denoting subdomain 1 (the matrix) and $(2)$ denoting subdomain 2 (the inclusions). Note that we need to restrict quantities since we work with a facet measure on the interface $\Gamma$. Although only one side exist for each subdomain, cells of a given subdomain from one side have been mapped to the other side, as discussed before. As a result, it does not really matter which side is used here. For consistency, we use the the `"+"` side for subdomain 1 and the `"-"` side for subdomain 2.
 
 
-# +
+# %%
 def jump(u1, u2):
     # As cell("+") are mapped to cell("-") when defining the cell maps, it does not really matter which side ("+"/"-") is used
     return u2("+") - u1("-")
@@ -280,25 +304,25 @@ def jump(u1, u2):
 K = fem.Constant(domain, 1e5)
 
 Res_interface = K * ufl.dot(jump(u1, u2), jump(v1, v2)) * dInt
-# -
 
+# %% [markdown]
 # ### Total residual and jacobian
 #
 # Finally, the total residual is the sum of all three residuals. Since we work with a `MixedFunctionSpace`, we use `ufl.extract_blocks` to extract the blocks corresponding to both `u1` and `u2`. We then compute the corresponding Jacobian with both `qmap.derivative` in the corresponding trial functions. Both the residual and tangent blocked forms are compiled by passing the `entity_maps` dictionary to `fem.form`.
 
-# +
+# %%
 Res = Res_matrix + Res_inclusions + Res_interface
 Res_blocked_compiled = fem.form(ufl.extract_blocks(Res), entity_maps=entity_maps)
 
 Jac = qmap1.derivative(Res, u1, du1) + qmap2.derivative(Res, u2, du2)
 Jac_blocked_compiled = fem.form(ufl.extract_blocks(Jac), entity_maps=entity_maps)
-# -
 
+# %% [markdown]
 # ### Boundary conditions
 #
 # We apply an imposed displacement on the right boundary and fix the left boundary. We use a virtual test field with unit value on the boundary to compute the consistent reaction force, see [](https://bleyerj.github.io/comet-fenicsx/tips/computing_reactions/computing_reactions.html).
 
-# +
+# %%
 Uimp = fem.Constant(domain, (1.0, 0.0))
 left_dofs = fem.locate_dofs_topological(V1, fdim, subdomain1_facet_tags.find(1))
 right_dofs = fem.locate_dofs_topological(V1, fdim, subdomain1_facet_tags.find(2))
@@ -316,13 +340,13 @@ virtual_work_form = fem.form(
     ufl.replace(Res, {v1: v_reac1, v2: v_reac2}),
     entity_maps=entity_maps,
 )
-# -
 
+# %% [markdown]
 # ## Resolution
 #
-# Next, we define the custom nonlinear problem. Here, we work with a blocked system and thus rely on `BlockedNonlinearMaterialProblem` which expects a list of behaviors to update and a list of functions corresponding to the blocked solution. We then define a classical Newton solver.
+# Next, we define the `NonlinearMaterialProblem` by passing the list of `QuadratureMap` objects and a list of functions corresponding to the blocked solution. We then define a classical Newton solver.
 
-# +
+# %%
 petsc_options = {
     "snes_type": "newtonls",
     "snes_linesearch_type": "none",
@@ -343,25 +367,18 @@ problem = NonlinearMaterialProblem(
     petsc_options=petsc_options,
 )
 
-# -
-
+# %% [markdown]
 # ### Time-stepping
 #
-# Upon time-stepping, post-processing steps are needed. First, the subdomain displacements `u1` and `u2` are reconstructed into a single `DG` displacement field `u` which is more convenient to handle in Paraview. Note that although this field is discontinuous on the whole mesh, non-zero jumps will occur only at the interface. Second, we recover plastic strain variables in both subdomains as `DG0` fields. Since this step involves a projection, we need to pass the `entity_maps` dictionary to the `project_on` method to compute the corresponding forms of the projection subproblem. Note that both plastic strains are not recombined into a single field, although it is possible in the present case.
+# Upon time-stepping, post-processing steps are needed. Fields associated with both domains are stored in their own file, since they do not share the same mesh.
 
-# + tags=["hide-output"]
-# file_results = io.VTKFile(
-#     domain.comm,
-#     f"multimaterials_results.pvd",
-#     "w",
-# )
-
+# %% tags=["hide-output"]
 N = 30
 Exx = np.linspace(0, 15e-3, N + 1)
 
 p1 = qmap1.project_on("EquivalentPlasticStrain", ("DG", 0), entity_maps=entity_maps)
 p1.name = "PlasticStrain1"
-p2 = qmap2.project_on("EquivalentPlasticStrain", ("DG", 0), entity_maps=entity_maps)
+p2 = qmap2.project_on("p", ("DG", 0), entity_maps=entity_maps)
 p2.name = "PlasticStrain2"
 
 
@@ -377,14 +394,10 @@ for i, exx in enumerate(Exx[1:]):
     problem.solve()
     assert problem.solver.getConvergedReason()
 
-    interpolate_submesh_to_parent(u, u1, subdomain1_cell_map)
-    interpolate_submesh_to_parent(u, u2, subdomain2_cell_map)
     qmap1.project_on(
         "EquivalentPlasticStrain", ("DG", 0), fun=p1, entity_maps=entity_maps
     )
-    p2 = qmap2.project_on(
-        "EquivalentPlasticStrain", ("DG", 0), fun=p2, entity_maps=entity_maps
-    )
+    p2 = qmap2.project_on("p", ("DG", 0), fun=p2, entity_maps=entity_maps)
 
     file1.write(i + 1)
     file2.write(i + 1)
@@ -393,13 +406,13 @@ for i, exx in enumerate(Exx[1:]):
 
 file1.close()
 file2.close()
-# -
 
+# %% [markdown]
 # ## Results
 #
 # We finally plot the resulting load-displacement curve.
 
-# +
+# %%
 import matplotlib.pyplot as plt
 
 plt.figure()
@@ -407,3 +420,10 @@ plt.plot(Exx, Force, "-oC3")
 plt.xlabel("Imposed horizontal strain")
 plt.ylabel("Reaction force")
 plt.show()
+
+# %%
+from dolfinx.common import list_timings
+
+list_timings(MPI.COMM_WORLD)
+
+# %%
